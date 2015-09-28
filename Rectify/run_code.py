0b7ca40279c3f224a52049b3aef73405454d22e5
dv-abhishek <dv.abhishek@gmail.com>
2015-09-28 11:40:47 +0530

    Added error message return

--- Rectify/run_code.py
+++ Rectify/run_code.py
@@ -21,23 +21,36 @@ def fun(code, user_code, input):
 	code_file.close()
 	##
 
-	not_compiled = sub.call(['gcc', '-o', 'user_code', 'user_code.c'])
-	if not_compiled:
-		return "Compilation Error"
-	sub.call(['gcc', '-o', 'code', 'code.c'])
+	#Compile User code
+	compilation_process = sub.Popen('gcc -o user_code user_code.c 2>error', shell=True)
+	exit_status = process.wait()
 
-	process = sub.Popen('./user_code < input >user_out', shell=True)
+	#Check for compilation error
+	if exit_status:
+		error_file = open('error')
+		error_message = error_file.read()
+		error_file.close()
+		return "Compilation Error", error_message
+
+	sub.call(['gcc', '-o', 'code', 'code.c'])		#Compile correct code
+
+	#Run user code
+	process = sub.Popen('./user_code < input 1>user_out 2>error', shell=True)
 
 	time.sleep(2)
-	if process.poll() is None:
+	if process.poll() is None:					#2 sec timeout period
 		process.terminate()
 		return "Time Limit Exceeded"
 
+	# If program exits before timeout, check for errors
 	exit_status = process.wait()
 	if exit_status:
-		return "Runtime Error"
+		error_file = open('error')
+		error_message = error_file.read()
+		error_file.close()
+		return "Runtime Error",error_message
 	
-	sub.call('./code < input >req_out', shell=True)
+	sub.call('./code < input >req_out', shell=True)		#Run correct code
 
 	different = sub.call('diff user_out req_out', shell=True)
 
